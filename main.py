498c343c6c549fda855aea49b2559fba0aa56a7f
KubaHyzicki <kuba.hyzicki@gmail.com>
2020-12-04 19:01:40 +0100

    define init for sheluder, improve argparse

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ def parse_arguments():
 
     parser.add_argument('--verbose', '-v', action = 'store_true', required = False, help = 'Sets logging type to DEBUG')
 
+    parser.add_argument('--tasks', required = True, help = 'Tasks amount')
+    duration_types = parser.add_mutually_exclusive_group()
+    duration_types.required = True
+    duration_types.add_argument('--duration_min_max', nargs = 2, help = 'Duration of a task random time frames, usage:\n--duration_min_max min_time max_time')
+    duration_types.add_argument('--durations', nargs = '*', help = 'Durations of tasks, usage:\n--durations duration_of_task_1 duration_of_task_2 ...')
+
+
 def main():
     args = parse_arguments()
 
-    sheluder = Sheluder()
-
     if args.verbose:
         src.sheluder.logger.setLevel(logging.DEBUG)
+
+    if args.duration_min_max:
+        sheluder = Sheluder(args.tasks, args.duration_min_max, randomize_durations = True)
+    elif args.durations:
+    sheluder = Sheluder(args.tasks, args.durations, randomize_durations = False)
 
     sheluder.run()
 
--- src/sheluder.py
+++ src/sheluder.py
@@ -1,7 +1,28 @@
 import logging
+import random
+
+from src.task import Task
 
 logging.basicConfig(level = "INFO")
 
 class Sheluder():
-    def __init__(self):
-        self.tasks = []
+    def __init__(self, tasks_amount, durations, randomize_durations):
+        if randomize_durations:
+            self.tasks = self.randomize_durations(tasks_amount, durations)
+        else:
+            self.tasks = self.define_durations(tasks_amount, durations)
+
+    def randomize_durations(self, tasks_amount, durations):
+        tasks = []
+        min, max = durations
+        for i in range(0, tasks_amount):
+            tasks.append(Task(id=i, run_time=randrange(min, max, 1)))
+        return tasks
+
+    def define_durations(self, tasks_amount, durations):
+        tasks = []
+        if not tasks_amount == len(durations):
+            logging.error("Tasks amount does not match durations list")
+        for i in range(0, tasks_amount):
+            tasks.append(Task(id=i, run_time=durations[i]))
+        return tasks
